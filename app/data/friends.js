c88144a0d6fe2772ddc5d18e818afd8703b3467d
emilyjreed <emilykjackson@gmail.com>
2018-05-22 15:32:29 -0400

    added to friend array

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,80 @@ var friendArray = [
         "name": "Louise Belcher",
         "photo": "https://res.cloudinary.com/teepublic/image/private/s--kQ2_oUBc--/t_Preview/b_rgb:484849,c_limit,f_jpg,h_630,q_90,w_630/v1516495946/production/designs/2294921_0.jpg",
         "scores": [
-            "1",
-            "3",
             "5",
-            "3",
+            "1",
+            "4",
+            "2",
             "2",
             "4",
             "5",
             "1",
             "2",
             "5"
+        ]
+    },
+    {
+        "name": "Linda Belcher",
+        "photo": "https://ih1.redbubble.net/image.145597300.6164/flat,800x800,075,f.u2.jpg",
+        "scores": [
+            "5",
+            "4",
+            "3",
+            "3",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Bob Belcher",
+        "photo": "https://res.cloudinary.com/teepublic/image/private/s--kQ2_oUBc--/t_Preview/b_rgb:484849,c_limit,f_jpg,h_630,q_90,w_630/v1516495946/production/designs/2294921_0.jpg",
+        "scores": [
+            "3",
+            "2",
+            "3",
+            "1",
+            "2",
+            "4",
+            "2",
+            "2",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "Tina Belcher",
+        "photo": "https://ih0.redbubble.net/image.214740398.5102/flat,800x800,075,f.jpg",
+        "scores": [
+            "1",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3",
+            "2",
+            "1",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Gene Belcher",
+        "photo": "https://cdn.pastemagazine.com/www/articles/best-quotes-gene-belcher-main.png",
+        "scores": [
+            "4",
+            "3",
+            "1",
+            "5",
+            "3",
+            "1",
+            "4",
+            "1",
+            "1",
+            "3"
         ]
     }
 ];
