ebc53264be0d51afc33d4880747fea45b3aa05ac
emilyjreed <emilykjackson@gmail.com>
2018-05-21 23:15:39 -0400

    survey data saved

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friendArray = [
     {
-        friendName: "Obi Wan Kenobi",
-        friendPhoto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fflx-editorial-wordpress%2Fwp-content%2Fuploads%2F2017%2F08%2F18143622%2FAlec-Guinness-Star-Wars.jpg&imgrefurl=https%3A%2F%2Feditorial.rottentomatoes.com%2Farticle%2Fdisney-announces-obi-wan-kenobi-spinoff-and-more-movie-news%2F&docid=Qt-3k8LO52TR5M&tbnid=YFX4nwxbEvmHaM%3A&vet=10ahUKEwjpoYbtsZfbAhXMuFMKHcQqCMcQMwjoAigoMCg..i&w=700&h=380&bih=680&biw=1280&q=obi%20wan%20kenobi&ved=0ahUKEwjpoYbtsZfbAhXMuFMKHcQqCMcQMwjoAigoMCg&iact=mrc&uact=8",
-        survey: [
+        "name": "Louise Belcher",
+        "photo": "https://www.google.com/search?q=louise+belcher&rlz=1C5CHFA_enUS791US791&source=lnms&tbm=isch&sa=X&ved=0ahUKEwivqKHKpJjbAhUNbq0KHduCDP4Q_AUICigB#",
+        "scores": [
             "1",
             "3",
             "5",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,15 @@
 var friends = require("../data/friends.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get('/api/friends', function(req, res){
-    return res.json(friends);
- });
+    app.get('/api/friends', function (req, res) {
+        return res.json(friends);
+    });
 
- 
- app.post('/api/clear', function(req, res) {
-     friends = [];
 
-     console.log(friends);
- });
-
+    app.post('/api/friends', function (req, res) {
+        var surveyData = req.body;
+        friends.push(surveyData);
+        res.json(friends);
+    });
 };
